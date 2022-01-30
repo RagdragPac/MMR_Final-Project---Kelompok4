744d373d57556208b570b7c210cf24852bc7768c
RagdragPac <ferdinanderfrans@gmail.com>
2022-01-30 23:45:27 +0700

    first commit

---
+++ Assets/RotateObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour {
+
+	public GameObject  objectRotate;
+	
+	public float	   rotateSpeed = 50f;
+	bool			   rotateStatus = true;
+
+	void Update() {
+		if (rotateStatus == true) {
+			objectRotate.transform.Rotate (Vector3.up, rotateSpeed * Time.deltaTime);
+		}
+	}
+}
